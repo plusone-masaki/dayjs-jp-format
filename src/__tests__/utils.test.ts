64345b74474623d6d0b8aa07551d909f20991c4f
m_enomoto <m_enomoto@plus-one.tech>
2022-01-27 16:53:58 +0900

    元号表記に非対応の端末が存在したため、フォールバック処理を追加

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,10 +1,10 @@
-import { getJpYear, getJpEra } from '../utils'
+import { getJpYear, getJpEra, findEra } from '../utils'
 
 describe('utils', () => {
   describe('getJpYear', () => {
     it('could convert the year correctly', () => {
       const actualDateString = '2020-06-24'
-      expect(getJpYear(new Date(actualDateString))).toBe('令和2年')
+      expect(getJpYear(new Date(actualDateString))).toBe('令和2')
     })
     it('Returns an error message when pass in an invalid date', () => {
       const actualDateString = '9999-99-99'
@@ -20,6 +20,25 @@ describe('utils', () => {
     it('Returns an error message when pass in an invalid date', () => {
       const actualDateString = '9999-99-99'
       expect(getJpEra(new Date(actualDateString))).toBe('不明')
+    })
+  })
+
+  describe('findEra', () => {
+    it('could convert the year correctly', () => {
+      const actualDateString = '1830-12-09'
+      expect(findEra(new Date(actualDateString))).toBe('文政')
+    })
+    it('could convert the year correctly', () => {
+      const actualDateString = '1830-12-10'
+      expect(findEra(new Date(actualDateString))).toBe('天保')
+    })
+    it('could convert the year correctly', () => {
+      const actualDateString = '1830-12-11'
+      expect(findEra(new Date(actualDateString))).toBe('天保')
+    })
+    it('Returns an error message when pass in an invalid date', () => {
+      const actualDateString = '600-12-31'
+      expect(findEra(new Date(actualDateString))).toBe('')
     })
   })
 })
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,11 @@
+import eras from './eras'
+
+export const findEra = (date: Date): string => {
+  const time = date.getTime()
+  const era = eras.find(e => time >= (new Date(e.from)).getTime())
+  return era?.name ?? ''
+}
+
 export const getJpYear = (date: Date): string => {
   let result: string
   try {
@@ -5,6 +13,10 @@ export const getJpYear = (date: Date): string => {
       year: '2-digit',
       era: 'long'
     }).format(date).slice(0, -1)
+
+    if (!isNaN(Number(result))) {
+      result = findEra(date) + result
+    }
   } catch {
     result = '該当なし'
   }
@@ -17,6 +29,10 @@ export const getJpEra = (date: Date): string => {
     result = Intl.DateTimeFormat('ja-JP-u-ca-japanese', {
       era: 'long'
     }).format(date).slice(0, 2)
+
+    if (!isNaN(Number(result))) {
+      result = findEra(date) + result
+    }
   } catch {
     result = '不明'
   }
